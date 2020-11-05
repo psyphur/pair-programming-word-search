c8d05342be012aa05b88cd55b98df3b37429a446
Shaun Yap <me@shaunyap.ca>
2020-11-05 00:03:14 +0000

    Fixed bugs

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+    // new const for vertical perhaps?
+    const verticalJoin = letters[0].map((x, colIndex) => letters.map(row => row[colIndex]).join(''));
+    const verticalJoinReverse = letters[0].map((x, colIndex) => letters.map(row => row[colIndex]).reverse().join(''));
+    const horizontalJoinReverse = letters.map(ls => ls.reverse().join(''));
+    console.log(verticalJoinReverse);
+
+    for (const l of horizontalJoinReverse) {
+        if(l.includes(word)) 
+            return true;
     }
+
+    for (const l of verticalJoinReverse) {
+        if(l.includes(word))
+            return true;
+    }
+
+    for (l of horizontalJoin) {
+        if (l.includes(word)) 
+            return true
+    }
+
+    for (l of verticalJoin) {
+        if (l.includes(word)) 
+            return true
+    }
+    return false;
 }
 
 module.exports = wordSearch
